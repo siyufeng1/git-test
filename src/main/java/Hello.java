e488e307667a7845bd29ec715efcd9aba4a5da84
siyufeng <3386001775@qq.com>
2023-08-04 17:16:59 +0800

    hot-fix-commit1

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
-        System.out.println(a + b);
+        int c = 30;
+        System.out.println(a + b + c);
     }
 }
