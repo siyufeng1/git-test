927b8c712f8e948377a3ee88ee985c99cfa51b1e
siyufeng <3386001775@qq.com>
2023-08-04 17:10:36 +0800

    third-commit

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,8 +2,9 @@
  * @author 司雨枫
  */
 public class Hello {
-   public static void main(String[] args) {
-      int a = 10;
-      System.out.println(a);
-   }
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println(a + b);
+    }
 }
